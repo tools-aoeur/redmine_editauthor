4307b7ddd6489ffdfaf7723962f348fea887b0d1
Jaap de Haan <jaap.dehaan@freenet.de>
2023-05-09 11:21:49 +0200

    feat: Redmine 5.0 support and cleanup (#1)

--- init.rb
+++ init.rb
@@ -1,12 +1,12 @@
-require File.expand_path('lib/redmine_editauthor', __dir__)
+require_relative 'lib/redmine_editauthor'
 
 Redmine::Plugin.register :redmine_editauthor do
-  name "Redmine Edit Issue Author"
-  author "Ralph Gutkowski"
-  description "Edit author of issue."
-  version '0.11.1'
-  url 'https://github.com/rgtk/redmine_editauthor'
-  author_url 'https://github.com/rgtk'
+  name "Edit Author"
+  author "Ralph Gutkowski (orig)"
+  description "Enables setting the author on issue edit or create, for allowed users."
+  version '1.0.0'
+  url 'https://github.com/tools-aoeur/redmine_editauthor'
+  author_url 'https://github.com/tools-aoeur'
 
   settings default: {
     'members_scope' => false
